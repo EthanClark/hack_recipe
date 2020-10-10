c6c9b4a2c9599a4a41edc4bfa064ac0dc7f0a1e2
Ethan Clark <clark.ethan.r@gmail.com>
2020-10-10 10:52:06 -0600

    cusines Controller

--- app/controllers/api/cuisines_controller.rb
+++ app/controllers/api/cuisines_controller.rb
@@ -1,2 +1,34 @@
 class Api::CuisinesController < ApplicationController
+
+  def index
+    render json: Cuisine.all
+  end
+
+  def create
+    cuisine = Cuisine.new(cuisine_params)
+    if cuisine.save
+      render json: cuisine
+    else 
+      render json: { errors: post.errors }, status:
+    end
+  end
+
+  def update
+    cuisine = Cuisine.find(params[:id])
+    if cuisine.update(cuisine_params)
+      render json: cuisine
+    else
+      render json: { errors: post.errors }, status:
+    end
+  end
+
+  def destroy
+    Cuisine.find(params[:id]).destroy
+    render json: { message: 'Cuisine deleted' }
+  end
+
+  private
+    def cuisine_params
+      params.require(:cuisines).permit(:cName)
+    end
 end
